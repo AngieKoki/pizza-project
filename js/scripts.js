11b67ec6067f5249465e7bfe469794edb9734fe1
Angie Koki <angie.dar66@gmail.com>
2020-08-01 22:05:36 +0300

    Create layout for script.js file

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//Order Now button
+
+
+// Order form
+
+    //Size of Pizza Manipulation
+
+    //Toppings Manipulation
+
+    //Crust Manipulation
+
+    //Multiple Pizza Manipulation
+
+    //Delivery Manipulation
+        //Location textarea
+        //Alert
+
+// Checkout button
