4c2d379f32af50cbfce710846cbfeb5d12137743
Angie Koki <angie.dar66@gmail.com>
2020-08-02 13:14:57 +0300

    Add jQuery functions to delivery manipulation section

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
+//BUSINESS LOGIC
+
+
+
+//FRONT END LOGIC
 //Order Now button
-
-
+$(document).ready(function(){
+    $('.orderNow').click(function(){
+        $('.orderForm').show();
+        $('.orderNow').slideUp();
+    });
+     $('#checkout').click(function(){
+        $('.orderNow').slideDown();
+        $('.orderForm').hide();
+    });
+});
+   
 // Order form
 
     //Size of Pizza Manipulation
@@ -11,7 +25,26 @@
 
     //Multiple Pizza Manipulation
 
-    //Delivery Manipulation
+//Delivery Manipulation
+document.getElementById('submit').addEventListener('click',
+function submit(event){
+    event.preventDefault();
+    var deliveryYes = document.getElementById('yes').value;
+    var deliveryNo = document.getElementById('no').value;
+    var locationForm=document.getElementById('deliveryLocation').value;
+    
+    if (deliveryYes ==='Yes'){
+        alert('Cost of delivery is 300/-.Fill in your location details');
+        prompt(locationForm);
+        alert('Your order will be delivered to ' + locationForm);
+    }
+    else if(deliveryNo ==='No'){
+        alert('The total cost of your order is ');
+    }
+    else {
+        alert('Please select yes/no to the delivery option');
+    }
+});
         //Location textarea
         //Alert
 
