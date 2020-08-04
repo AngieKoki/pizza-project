1cdd5ba26802a878207e90cdc741f0445508705e
Angie Koki <angie.dar66@gmail.com>
2020-08-04 07:52:48 +0300

    debug js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         $('.orderForm').toggle();
         $('.orderNow').slideUp();
      });
+  });
+  $(document).ready(function(){
     $('#pizzaform').submit(function(event) {
         event.preventDefault();
         function size() {
@@ -38,22 +40,21 @@ $(document).ready(function(){
     
         var userInput = new Order(size(), crust(), topping(), number(), delivery());
     
-        //a variable to store the total expenditure of the user
         var totalCost =
           (userInput.newSize +
             userInput.newCrust +
             userInput.newTopping) *
             userInput.newQuantity;
     
-        //prompts for the user
         if(pizzaDelivery===2){
-            alert(userInput());
+            alert('Your order is: '+ userInput());
             alert('Your total is: ' + totalCost);
             alert('Enjoy your Pizza!')
         }
         else if(pizzaDelivery===1){
             var location=prompt('Input delivery location')
             alert('Your pizza will be delivered to ' + location)
+            alert('Your order is: '+ userInput());
             alert('Your total is: ' + totalCost);
             alert('Thank you! Your order will arrive in an hour')
         }
@@ -134,63 +135,4 @@ $(document).ready(function(){
 //         }
 //     });
 // });
-// $(document).ready(function() {
-//       $('#pizzaform').submit(function(event) {
-//         function size() {
-//           var pizzaSize = document.getElementById("sizePizza").value;
-//           return parseInt(pizzaSize);
-//         }
-//         function crust() {
-//           var pizzaCrust = document.getElementById("crust").value;
-//           return parseInt(pizzaCrust);
-//         }
-//         function topping() {
-//           var pizzaTopping = document.getElementById("topping").value;
-//           return parseInt(pizzaTopping);
-//         }
-//         function number() {
-//           var pizzaNumber = document.getElementById("quantity").value;
-//           return parseInt(pizzaNumber);
-//         }
-//         function delivery() {
-//             var pizzaDelivery = document.getElementById("delivery").value;
-//             return parseInt(pizzaDelivery);
-//         }
-    
-//         function Order(size, crust, topping, quantity, delivery) {
-//           this.newSize = size;
-//           this.newCrust = crust;
-//           this.newTopping = topping;
-//           this.newQuantity = quantity;
-//           this.newDelivery = delivery;
-          
-//         }
-    
-//         var userInput = new Order(size(), crust(), topping(), number(), delivery());
-    
-//         //a variable to store the total expenditure of the user
-//         var totalCost =
-//           (userInput.newSize +
-//             userInput.newCrust +
-//             userInput.newTopping) *
-//             userInput.newQuantity;
-    
-//         //prompts for the user
-//         if(pizzaDelivery===2){
-//             alert(userInput());
-//             alert('Your total is: ' + totalCost);
-//             alert('Enjoy your Pizza!')
-//         }
-//         else if(pizzaDelivery===1){
-//             var location=prompt('Input delivery location')
-//             alert('Your pizza will be delivered to ' + location)
-//             alert('Your total is: ' + totalCost);
-//             alert('Thank you! Your order will arrive in an hour')
-//         }
-//         //a method to reset the form after all operations have been completed
-//         // $("#pizzaForm").reset();
-    
-//         event.preventDefault();
-//       });
-//     });
 
